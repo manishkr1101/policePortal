8db93a5e290def28e9d756a92916a554a968a506
khmr-2622 <intelligentharsh007@gmail.com>
2020-01-14 23:30:38 +0530

    criminal link added

--- api/routes/pdfGeneration.js
+++ api/routes/pdfGeneration.js
@@ -1,19 +1,24 @@
-var fs= require("fs");
-var pdfkit= require("pdfkit");
-doc = new PDFDocument
-doc.pipe fs.createWriteStream('output.pdf')
-doc.font('fonts/PalatinoBold.ttf')
-   .fontSize(25)
-   .text('Some text with an embedded font!', 100, 100);
- 
-doc.image('path/to/image.png', {
-   fit: [250, 300],
-   align: 'center',
-   valign: 'center'
-});
- 
-doc.addPage()
-   .fontSize(25)
-   .text('Here is some vector graphics...', 100, 100)
- 
-doc.end()
+var fs = require("fs");
+var PDFDocument = require("pdfkit");
+doc = new PDFDocument();
+doc.pipe = fs.createWriteStream("output.pdf");
+doc
+  .font("Times-Roman")
+  .fontSize(25)
+  .text("Some text with an embedded font!", 100, 100);
+
+doc.image(
+  "https://web.whatsapp.com/pp?e=https%3A%2F%2Fpps.whatsapp.net%2Fv%2Ft61.24694-24%2F69386955_249748782612395_805351971962748928_n.jpg%3Foe%3D5E20E5B8%26oh%3D5f1790a2d8cc417bdd13c0071d777816&t=s&u=917070584769%40c.us&i=1570550130",
+  {
+    fit: [250, 300],
+    align: "center",
+    valign: "center"
+  }
+);
+
+doc
+  .addPage()
+  .fontSize(25)
+  .text("Here is some vector graphics...", 100, 100);
+
+doc.end();
--- api/routes/records/criminals.js
+++ api/routes/records/criminals.js
@@ -1,36 +1,36 @@
 const express = require("express");
 const router = express.Router();
-const criminals = require('../../middleware/criminals')
-const formidable = require('formidable')
+const criminals = require("../../middleware/criminals");
+const formidable = require("formidable");
 
 router.get("/", (req, res) => {
-    criminals.getAllCriminals()
-        .then(doc => {
-            res.render("records/criminal", {
-                title: "Criminals Records",
-                user: getUser(req),
-                css: 'criminal',
-                criminals: doc
-            });
-        })
-
+  criminals.getAllCriminals().then(doc => {
+    res.render("records/criminal", {
+      title: "Criminals Records",
+      user: getUser(req),
+      css: "criminal",
+      criminals: doc
+    });
+  });
 });
 
-router.post('/search', (req, res) => {
-    let form = new formidable.IncomingForm()
-    form.parse(req, (err, fields, files) => {
-        res.send({
-            fie: fields
-        })
-    })
-})
+router.get("/criminals/id", (req, res) => {});
+
+router.post("/search", (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.parse(req, (err, fields, files) => {
+    res.send({
+      fie: fields
+    });
+  });
+});
 
 function getUser(req) {
-    return {
-        name: (req.user && req.user.name) || 'Manish',
-        email: `${(req.user && req.user.username) || 'manishkr7424'}@gmail.com`,
-        imgUrl: (req.user && req.user.imgUrl) || '/images/logo.png'
-    }
+  return {
+    name: (req.user && req.user.name) || "Manish",
+    email: `${(req.user && req.user.username) || "manishkr7424"}@gmail.com`,
+    imgUrl: (req.user && req.user.imgUrl) || "/images/logo.png"
+  };
 }
 
-module.exports = router
+module.exports = router;
